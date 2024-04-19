feef454ecc320cb28bb6f47cbaf380fa28bcff6b
Mohammadawad1 <mohammadawad375@gmail.com>
2024-04-19 15:49:18 +0300

    edit

---
+++ AnsibeProject/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AnsibeProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View( );
+        }
+    }
+}
