4952789a488b2aad466e1b9f4cd3855a138ec7b0
Mohammed Bilal <mohammed.bilalgr@gmail.com>
2024-04-20 14:52:09 +0300

    Add the Course controller

--- AnsibeProject/Controllers/CourseController.cs
+++ AnsibeProject/Controllers/CourseController.cs
@@ -1,12 +1,124 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AnsibeProject.Controllers.CourseWork;
+using AnsibeProject.Data;
+using AnsibeProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 
 namespace AnsibeProject.Controllers
 {
     public class CourseController : Controller
     {
+        private readonly Courses courses;
+        public CourseController(UniversityContext universityContext)
+        {
+            courses = new Courses(universityContext);
+        }
         public IActionResult Index()
         {
-            return View();
+            return View(courses.getAllCourses());
+        }
+        public IActionResult Edit(string CourseCode)
+        {
+            if (! CourseCode.IsNullOrEmpty()) 
+            {
+                Course courseToUpdate;
+                try
+                {
+                    courseToUpdate = courses.getCourseByCode(CourseCode);
+                    return View("Edit",courseToUpdate);
+                }catch(Exception ex)
+                {
+                    ModelState.AddModelError("", "an error accourse while getting corse by code");
+                    ModelState.AddModelError("",ex.Message);
+                }
+            }else
+            {
+                ModelState.AddModelError("", "failed to get courseCode String null or empty  issue");
+            }
+            return RedirectToAction(nameof(Index));
+        }
+        [HttpPost]
+        public IActionResult Edit(Course courseToEdit)
+        {
+            if (courseToEdit != null)
+            {
+                if (ModelState.IsValid)
+                {
+                    try
+                    {
+                        courses.UpdateCourse(courseToEdit);
+                        return RedirectToAction(nameof(Index));
+                    }
+                    catch (Exception ex)
+                    {
+                        ModelState.AddModelError("", "failed to add Course");
+                        ModelState.AddModelError("", ex.Message);
+                    }
+                }
+                else
+                {
+                    return View("Edit", courseToEdit);
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("", "failed to get course instance null issue");
+            }
+            return View("Add", courseToEdit);
+        }
+        public IActionResult AddCourse()
+        {
+            return View("Add");
+        }
+        [HttpPost]
+        public IActionResult AddCourse(Course courseToAdd) 
+        {
+            if (courseToAdd != null) 
+            {
+                if(ModelState.IsValid)
+                {
+                    try
+                    {
+                        courses.AddCourse(courseToAdd);
+                        return RedirectToAction(nameof(Index));
+                    }catch (Exception ex)
+                    {
+                        ModelState.AddModelError("", "failed to add Course");
+                        ModelState.AddModelError("", ex.Message);
+                    }
+                }
+                else
+                {
+                    return View("Add", courseToAdd);
+                }
+            }else
+            {
+                ModelState.AddModelError("", "failed to get course instance null issue");
+            }
+            return View("Add", courseToAdd);
+        }
+        public IActionResult Delete(string CourseCode) 
+        {
+            if(! CourseCode.IsNullOrEmpty())
+            {
+                try
+                {
+                    courses.DeleteCourse(CourseCode);
+
+                }
+                catch (Exception ex)
+                {
+                    ModelState.AddModelError("", "An error accoures while deleting the Course");
+                    ModelState.AddModelError("", ex.Message);
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("", "failed to get course instance null or empty issue");
+            }
+
+            return RedirectToAction(nameof(Index));
+
         }
     }
 }
---
+++ AnsibeProject/Controllers/CourseWork/Courses.cs
@@ -0,0 +1,51 @@
+﻿using AnsibeProject.Data;
+using AnsibeProject.Models;
+
+namespace AnsibeProject.Controllers.CourseWork
+{
+    public class Courses
+    {
+        private readonly UniversityContext  universityContext;
+        public Courses(UniversityContext universityContext)
+        {
+            universityContext = universityContext;
+        }
+
+        public Course getCourseByCode(string CourseCode) 
+        {
+            return universityContext.Courses.Find(CourseCode);  
+        }
+        public List<Course> getAllCourses() 
+        {
+            return universityContext.Courses.ToList();
+        }
+        public void AddCourse(Course course) 
+        {
+            universityContext.Courses.Add(course);
+            universityContext.SaveChanges();
+        }
+        public void UpdateCourse(Course course)
+        {
+            universityContext.Courses.Update(course);
+            universityContext.SaveChanges();
+        }
+        public void DeleteCourse(String CourseCode)
+        {
+
+            universityContext.Courses.Remove(universityContext.Courses.Find(CourseCode));
+            universityContext.SaveChanges();
+        }
+        /*
+         * use this methods when we want to show the courses that are ready to bind with teacher
+         */
+        public List<Course> getActiveCourses()
+        {
+            return universityContext.Courses.Where(x => x.CourseState == ActiveState.Active).ToList();
+        }
+        
+        public List<Course> getCoursesOfMajor(CourseMajor major)
+        {
+            return universityContext.Courses.Where(x => x.Major == major).ToList();
+        }
+    }
+}
