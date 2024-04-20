1c7e84e83f23ec99cf9782d83e3cb5cc8e121f68
Mohammadawad1 <mohammadawad375@gmail.com>
2024-04-20 14:54:31 +0300

    index /Add/edit

--- AnsibeProject/Controllers/CourseController.cs
+++ AnsibeProject/Controllers/CourseController.cs
@@ -1,12 +1,2 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
-namespace AnsibeProject.Controllers
-{
-    public class CourseController : Controller
-    {
-        public IActionResult Index()
-        {
-            return View();
-        }
-    }
-}
