14c93db3d1e654421dfd4e04d29653448dd25ca7
omar1z <omarzammar11@gmail.com>
2024-05-12 00:50:19 +0300

    CreateSection is now ready, it's working but we have a weird "System.Collections.Generic.List`1[AnsibeProject.Models.Course] List" in the top of the page ... please check it out !

--- AnsibeProject/Controllers/HomeController.cs
+++ AnsibeProject/Controllers/HomeController.cs
@@ -1,12 +1,30 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AnsibeProject.Data;
+using AnsibeProject.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AnsibeProject.Controllers
 {
     public class HomeController : Controller
+
     {
+        private readonly UniversityContext _db;
+        public HomeController(UniversityContext db) {
+            _db = db;
+        }
         public IActionResult Index()
         {
             return View( );
         }
+        public IActionResult CreateAnsibe()
+        {
+            return View();
+        }
+        public IActionResult CreateSection()
+        {
+           List<Course> courses = _db.Courses.Where(c => c.CourseState == Models.ActiveState.Active).ToList();
+            ViewBag.courses = courses;
+            return View(courses);
+        }
+
     }
 }
