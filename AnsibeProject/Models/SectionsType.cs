e3fdb68cd69e0a4ab5ff9045933a936e32ef7a8b
AnasAljaour <anas.aljaour13@gmail.com>
2024-06-29 21:54:10 +0300

    add delete when choosing another Ansibe ,fix view in home index edit sectionType model with some fix in AssignPController

--- AnsibeProject/Models/SectionsType.cs
+++ AnsibeProject/Models/SectionsType.cs
@@ -2,7 +2,6 @@
 {
     public class SectionsType
     {
-        public List<Section>? Sections { get; set; } 
         public List<Section>? TempSections { get; set; } 
         public string Type {  get; set; } = string.Empty;
         public string AnsibeId { get; set; }
