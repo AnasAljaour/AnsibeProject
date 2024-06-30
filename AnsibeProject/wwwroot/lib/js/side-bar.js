fc0aaddfe1333a6a1a8543bf734844b789d79599
Mohammadawad1 <mohammadawad375@gmail.com>
2024-06-30 22:15:58 +0300

    ok4

--- AnsibeProject/wwwroot/lib/js/side-bar.js
+++ AnsibeProject/wwwroot/lib/js/side-bar.js
@@ -1,1 +1,79 @@
 ﻿
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    let sideBar = document.querySelector('.side-bar');
+    let button = document.querySelector('#menu-btn');
+    handleResize();
+    window.addEventListener('resize', handleResize);
+    button.addEventListener('click', showSidebar);
+});
+
+
+function showSidebar() {
+    let saidebar = document.querySelectorAll('.side-bar');
+    var s = document.querySelectorAll('.Assign');
+
+    if (saidebar[0].classList.contains('active')) {
+        saidebar[0].classList.remove('active');
+        if (window.innerWidth > 1200 && !saidebar[1].classList.contains('active'))
+            saidebar[1].classList.add('active');
+
+        s.forEach(function (element) {
+            if (element.classList.contains('courses'))
+                element.classList.remove('courses');;
+        });
+    }
+    else {
+        saidebar[0].classList.add('active');
+        /* if (window.innerWidth > 1200 && !saidebar[1].classList.contains('active')) {
+             saidebar[1].classList.add('active');
+         }*/
+        s.forEach(function (element) {
+            if (!element.classList.contains('courses'))
+                element.classList.add('courses');
+        });
+    }
+
+
+
+
+}
+
+
+function handleResize() {
+    let sideBar = document.querySelectorAll('.side-bar');
+    var s = document.querySelectorAll('.Assign');
+    if (window.innerWidth < 1200) {
+
+        sideBar.forEach(function (element) {
+            if (element.classList.contains('active'))
+                element.classList.remove('active');
+        });
+
+
+    }
+    else {
+
+        if (sideBar[0].classList.contains('active')) {
+            sideBar[0].classList.remove('active');
+            sideBar[1].classList.add('active');
+
+        }
+        else {
+            sideBar[1].classList.add('active');
+        }
+
+        s.forEach(function (element) {
+            if (element.classList.contains('courses'))
+                element.classList.remove('courses');
+        });
+
+    }
+
+}
+function showDropdown() {
+
+    let saidebar = document.querySelectorAll('.side-bar');
+    saidebar[0].classList.add('active');
+    saidebar[1].classList.remove('active');
+}
