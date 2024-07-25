ac5d1d3c08cfab6fdacc44cb2a8a8a2d45258f07
Mohammed Bilal <mohammed.bilalgr@gmail.com>
2024-07-26 00:05:39 +0300

    Read Date From Excel

--- AnsibeProject/Controllers/CourseWork/ExcelReader.cs
+++ AnsibeProject/Controllers/CourseWork/ExcelReader.cs
@@ -2,6 +2,8 @@
 
 using AnsibeProject.Data;
 using AnsibeProject.Models;
+using ClosedXML.Excel;
+using DocumentFormat.OpenXml.Spreadsheet;
 using Microsoft.AspNetCore.Http;
 using Microsoft.IdentityModel.Tokens;
 using OfficeOpenXml;
@@ -40,44 +42,57 @@ public class ExcelReader<T> where T:new()
 
                 for (int col = 1; col <= worksheet.Dimension.Columns; col++)
                 {
-                    var myAttribute = typeof(T).GetProperty(worksheet.Cells[1, col].Value?.ToString());
-                    var cell = worksheet.Cells[row, col].Value?.ToString();
-                    
-                    if (myAttribute != null)
+                    try
                     {
-                        if (myAttribute.PropertyType.IsEnum)
-                        {
-                            Regex arabicRegex = new Regex(@"\p{IsArabic}");
-                            if (arabicRegex.IsMatch(cell))
-                            {
+                        var myAttribute = typeof(T).GetProperty(worksheet.Cells[1, col].Value?.ToString());
+                        var cell = worksheet.Cells[row, col].Value?.ToString();
 
-                                if (arabicToEnglish.ContainsKey(cell))
-                                {
-                                    cell = arabicToEnglish[cell];
-                                }else
-                                {
-                                    continue;
-                                }
-
-                            }
-                            try {
-                                object myEnum = Enum.Parse(myAttribute.PropertyType, cell);
-                                myAttribute.SetValue(dataObject, myEnum);
-                            }
-                            catch (Exception ex) { }
-                        } else if (myAttribute.PropertyType == typeof(Models.Contract))
+                        if (myAttribute != null && cell != null)
                         {
-                            try
+                            if (myAttribute.PropertyType.IsEnum)
                             {
-                                var contract = _universityContext.Contracts.FirstOrDefault(c => c.ContractType == cell);
-                                if (contract != null)
+                                Regex arabicRegex = new Regex(@"\p{IsArabic}");
+                                if (arabicRegex.IsMatch(cell))
                                 {
-                                    System.Diagnostics.Debug.WriteLine("Contract found: " + contract.ContractType);
-                                    myAttribute.SetValue(dataObject, contract);
+
+                                    if (arabicToEnglish.ContainsKey(cell))
+                                    {
+                                        cell = arabicToEnglish[cell];
+                                    } else
+                                    {
+                                        continue;
+                                    }
+
                                 }
-                                else
+                                try {
+                                    object myEnum = Enum.Parse(myAttribute.PropertyType, cell);
+                                    myAttribute.SetValue(dataObject, myEnum);
+                                }
+                                catch (Exception ex) { }
+                            } else if (myAttribute.PropertyType == typeof(Models.Contract))
+                            {
+                                try
                                 {
-                                    System.Diagnostics.Debug.WriteLine("Contract not found, setting default values.");
+                                    var contract = _universityContext.Contracts.FirstOrDefault(c => c.ContractType == cell);
+                                    if (contract != null)
+                                    {
+                                        System.Diagnostics.Debug.WriteLine("Contract found: " + contract.ContractType);
+                                        myAttribute.SetValue(dataObject, contract);
+                                    }
+                                    else
+                                    {
+                                        System.Diagnostics.Debug.WriteLine("Contract not found, setting default values.");
+                                        myAttribute.SetValue(dataObject, new Models.Contract()
+                                        {
+                                            ContractType = "failed to load contract",
+                                            MaxHours = 0,
+                                            MinHours = 0,
+                                            Professors = new List<Models.Professor>()
+                                        });
+                                    }
+                                }
+                                catch (Exception ex)
+                                {
                                     myAttribute.SetValue(dataObject, new Models.Contract()
                                     {
                                         ContractType = "failed to load contract",
@@ -86,48 +101,49 @@ public class ExcelReader<T> where T:new()
                                         Professors = new List<Models.Professor>()
                                     });
                                 }
-                            }
-                            catch (Exception ex)
+                            } else if (myAttribute.PropertyType == typeof(DateOnly))
                             {
-                                myAttribute.SetValue(dataObject, new Models.Contract()
+                                string dateFormat = "dd/mm/yyyy"; // Adjust the format according to your Excel file
+
+
+                                /*myAttribute.SetValue(dataObject,DateOnly
+                                    .FromDateTime(DateTime
+                                    .FromOADate(Convert
+                                    .ToDouble(cell))
+                                    .Date));*/
+                                try
                                 {
-                                    ContractType = "failed to load contract",
-                                    MaxHours = 0,
-                                    MinHours = 0,
-                                    Professors = new List<Models.Professor>()
-                                });
+                                    string[] dateTimeParts = cell.Split(' ');
+                                    string datePart = dateTimeParts[0]; // Take the first part which should be the 
+                                    // Try to convert the cell value to a double
+                                    double oaDate = Convert.ToDouble(datePart);
+                                    // Convert the OLE Automation date to DateOnly
+                                    myAttribute.SetValue(dataObject, DateOnly.FromDateTime(DateTime.FromOADate(oaDate)));
+                                }
+                                catch (Exception e)
+                                {
+                                    myAttribute.SetValue(dataObject, DateOnly.MaxValue);
+                                }
                             }
-                        }else if(myAttribute.PropertyType == typeof(DateOnly))
-                        {
-                            string dateFormat = "dd/mm/yyyy"; // Adjust the format according to your Excel file
-                            
-                            //DateOnly dateOnly = DateOnly.ParseExact(cell, "dd/MM/yyyy", CultureInfo.InvariantCulture);
-                            //DateTime.FromOADate(Convert.ToDouble(cell));
-                            myAttribute.SetValue(dataObject,DateOnly.FromDateTime(DateTime.FromOADate(Convert.ToDouble(worksheet.Cells[row, col].Value)).Date));
-                            /*try
+                            else
                             {
-                                DateOnly dateOnly = DateOnly.ParseExact(cell, "dd/MM/yyyy", CultureInfo.InvariantCulture);
-                                myAttribute.SetValue(dataObject, dateOnly);
+                                try
+                                {
+                                    myAttribute.SetValue(dataObject, Convert.ChangeType(cell, myAttribute.PropertyType));
+                                } catch (Exception ex) { }
                             }
-                            catch(Exception e)
-                            {
-                                myAttribute.SetValue(dataObject, DateOnly.MaxValue);
-                            }*/
+
+
                         }
-                        else
-                        {
-                            try
-                            {
-                                myAttribute.SetValue(dataObject, Convert.ChangeType(cell, myAttribute.PropertyType));
-                            }catch(Exception ex) { }
-                        }
-                        
-                        
+                        // Read cell value and add it to the list
+                        //var cellValue = worksheet.Cells[row, col].Value?.ToString();
+                        dataList.Add(dataObject);
+                    }catch(Exception e)
+                    {
+                        continue;
                     }
-                    // Read cell value and add it to the list
-                    //var cellValue = worksheet.Cells[row, col].Value?.ToString();
-                    dataList.Add(dataObject);
-                }
+                    }
+                    
             }
         }
 
